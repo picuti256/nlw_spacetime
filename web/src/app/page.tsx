124bb598c523866a3afb315a09b3c12da6fa8da1
picuti256 <picuti256@gmail.com>
2023-05-16 11:26:48 -0300

    Criação inicial do front end

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "../../Components/Button";
+
+export default function Home() {
+	return (
+		<h1>
+			Hello World
+			<Button />
+		</h1>
+	);
+}
