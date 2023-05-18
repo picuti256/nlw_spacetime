5796d666403b005eb4739ceb7e7fc783398d5d23
picuti256 <picuti256@gmail.com>
2023-05-18 10:30:05 -0300

    configurações do tailwind em relação de cores e fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
-	return (
-		<div className="text-2xl font-bold uppercase">
-			oi
-		</div>
-	);
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua capsula do tempo</h1>
+      <h1 className="font-alt text-4xl font-bold">Sua capsula do tempo</h1>
+    </div>
+  );
 }
