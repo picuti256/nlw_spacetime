ce07846c469a2722e292bfc6499b61bec09029f6
picuti256 <picuti256@gmail.com>
2023-05-20 15:33:14 -0300

    Ajuste stripes da pagina

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -51,7 +51,7 @@ module.exports = {
       },
       backgroundImage: {
         stripes:
-          "linear-gradiet(to bottom, rgba(255,255,255, 0.1),rgba(255,255,255, 0.1) 12.5%, transparent 12.5%, transparent)",
+          "linear-gradient(to bottom, rgba(255,255,255, 0.1),rgba(255,255,255, 0.1) 12.5%, transparent 12.5%, transparent)",
       },
       backgroundSize: {
         stripes: "100% 8px",
